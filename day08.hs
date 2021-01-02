2fdb468f241d8b9f4eb21eaf4f64ba3da9748f77
Sam M <sam.mackenzie@live.com>
2021-01-02 17:51:29 +1300

    added day 8

---
+++ day08.hs
@@ -0,0 +1,49 @@
+parseParam :: String -> Int
+parseParam all@(x:xs) 
+    | x == '+' = read xs
+    | otherwise = read all
+
+parseLine :: (Int, String) -> (Int, (String, Int))
+parseLine (ip, l) = (ip, (i, parseParam p))
+    where [i,p] = words l
+          
+readInput :: IO [(Int, (String, Int))]
+readInput = do
+    content <- readFile "day08.txt"
+    return $ map parseLine $ zip [0..] $ lines content
+
+runInstruction :: (Int, Int) -> (String, Int) -> (Int, Int)
+runInstruction (ip, acc) is = 
+    case cmd of "nop" -> (ip+1, acc)
+                "acc" -> (ip+1, acc+arg)
+                "jmp" -> (ip+arg, acc)
+    where (cmd,arg) = is
+
+emulateCode :: (Int, Int) -> [Int] -> [(Int, (String, Int))] -> (Int, Int)
+emulateCode (ip, acc) sf isx 
+    | ip `elem` sf = (ip, acc)
+    | otherwise = case (lookup ip isx) of 
+        Just ni -> emulateCode (runInstruction (ip, acc) ni) (ip:sf) isx
+        Nothing -> (ip, acc)
+
+swapInstruction :: Int -> [(Int, (String, Int))] -> [(Int, (String, Int))]
+swapInstruction ip (c:cs)
+    | ip == isp = (isp, (newc, arg)):cs
+    | otherwise = c : (swapInstruction ip cs)
+    where (isp, (cmd,arg)) = c
+          newc = case cmd of 
+                "nop" -> "jmp"
+                "jmp" -> "nop"
+    
+part1 :: IO Int
+part1 = do
+    inputData <- readInput
+    return . snd $ emulateCode (0,0) [] inputData
+
+part2 :: IO Int
+part2 = do
+    inputData <- readInput
+    return $ head [
+        acc | tip <- [ip | (ip, (cmd,_)) <- inputData, cmd `elem` ["jmp", "nop"]], -- Get list of instruction pointers for jmp and nop instructions
+        let (ip,acc) = emulateCode (0,0) [] $ swapInstruction tip inputData, -- emulate each set of code with the instructons swapped
+        ip == length inputData] -- Filter permutations to the ones that have an instruction pointer that is at the end of the code when completed
